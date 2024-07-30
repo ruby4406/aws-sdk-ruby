aadd169cf871be13e59415af88fb79056e239349
ruby4406 <163713192+ruby4406@users.noreply.github.com>
2024-07-31 03:57:48 +0530

    Revert "fix-config-parsing"

--- gems/aws-sdk-core/lib/aws-sdk-core/profile.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'profile/profile'
-require_relative 'profile/profile_file'
-require_relative 'profile/profile_file_factory'
-require_relative 'profile/profile_file_parser'
-require_relative 'profile/profile_file_standardizer'
-require_relative 'profile/profile_file_utils'
